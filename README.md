069395725d02a15fc08835bf9eb31ac4b8d17f63
Aleksey <pyksik@gmail.com>
2020-10-14 16:26:07 +0300

    Init vue-if-device

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # vue-if-device
+
+**vue-if-device** is a simple vue plugin to show/hide node on mobile/desktop devices.
+
+### Install
+
+```cmd
+npm install --save vue-if-device
+```
+
+### Quick Start
+
+```js
+import device from 'vue-if-device'
+Vue.use(device)
+```
+
+### Methods
+
+- v-if-device.mobile
+- v-if-device.desktop.
+
+
+
+## License
+MIT - Created by Aleksey Zimin (@zimingit)
---
+++ vue-if-device.js
@@ -0,0 +1,28 @@
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+
+const device = {
+  /**
+   * modifiers: desktop/mobile
+   */
+  bind (el, { modifiers }) {
+    // Чтобы не "моргало"
+    const { desktop, mobile } = modifiers
+    // Если мобильное устройство, а модификатор desktop, то скрываем
+    if (isMobile && desktop) el.style.display = 'none'
+    // Если десктоп, а модификатор mobile, то скрываем
+    if (!isMobile && mobile) el.style.display = 'none'
+  },
+  inserted (el, { modifiers }) {
+    const { desktop, mobile } = modifiers
+    // Если мобильное устройство, а модификатор desktop, то удаляем
+    if (isMobile && desktop) el.remove()
+    // Если десктоп, а модификатор mobile, то удаляем
+    if (!isMobile && mobile) el.remove()
+  }
+}
+
+export default {
+  install (Vue) {
+    Vue.directive('if-device', device)
+  }
+}
